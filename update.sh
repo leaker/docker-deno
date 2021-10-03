a906c2d49a8a522fb1c4804bd1d97704df70a155
leaker <admin@leelib.com>
2021-10-03 08:20:52 +0700

    fix output

--- update.sh
+++ update.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
-function get_verson() {
+function make_latest() {
   local ver=`curl -s https://api.github.com/repos/denoland/deno/releases/latest | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'`
-  echo "last version: $ver"
+  # echo "last version: $ver"
   local version_root=`echo $ver | grep -Eo "[0-9]+" | head -n 1`
-  echo "root: ${version_root}"
+  # echo "root: ${version_root}"
   local docker_folder="${version_root}/$ver"
-  echo "docker_folder: $docker_folder"
+  # echo "docker_folder: $docker_folder"
   # docker_folder+="/Dockerfile"
   local docker_file='Dockerfile'
-  local v1='aaaaaa'
   local docker_path=$version_root/$ver/$docker_file
   echo $docker_path
-  if [ -d "$docker_path" ]; then
-    echo 'exist'
+  if [ -f ./$docker_path ]; then
     echo "::set-output name=updated::0"
-    exit 0
+    return
   fi
-  echo 'not exist'
   mkdir -p $docker_folder
   echo 'FROM ubuntu:latest
 WORKDIR /root
@@ -28,5 +25,4 @@ ENTRYPOINT ["deno"]
   echo "::set-output name=updated::1"
 }
 
-get_verson
-
+make_latest
